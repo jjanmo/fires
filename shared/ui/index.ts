83aed151d388a7b8a2fe1ad679a76c41f267d36c
jjanmo <jjanmo07@hanmail.net>
2026-05-05 01:02:16 +0900

    fix: DotsPending 컴퍼넌트 export 방식 변경2

--- shared/ui/index.ts
+++ shared/ui/index.ts
@@ -1,7 +1,7 @@
-export { default as ThemeProvider } from './ThemeProvider'
-export { AuthProvider } from './AuthProvider'
-export { default as ThemeToggle } from './ThemeToggle'
-export { default as Logo } from './Logo'
-export { default as HeaderSearch } from './HeaderSearch'
-export { default as InfoTooltip } from './InfoTooltip'
-export { DotsPending } from './DotsPending'
+export { default as ThemeProvider } from './ThemeProvider';
+export { AuthProvider } from './AuthProvider';
+export { default as ThemeToggle } from './ThemeToggle';
+export { default as Logo } from './Logo';
+export { default as HeaderSearch } from './HeaderSearch';
+export { default as InfoTooltip } from './InfoTooltip';
+export { default as DotsPending } from './DotsPending';
